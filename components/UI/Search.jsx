0a631926dcd5ae7c6822f1bb1ef6414d0f3450d1
190101101 <190101101@mail.ru>
2023-09-21 01:27:58 +0400

    13 Header Responsive

--- components/UI/Search.jsx
+++ components/UI/Search.jsx
@@ -7,10 +7,10 @@ const Search = ({isSearchModal, setIsSearchModal}) => {
   return (
     <div className="fixed w-screen h-screen z-50 left-0 top-0 
     after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute
-    after:top-0 after:left-0 after:opacity-50 grid place-content-center">
+    after:top-0 after:left-0 after:opacity-0 grid place-content-center">
       <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
         <div className='w-full h-full grid place-content-center'>
-          <div className="relative z-50 w-[600px] h-[600px] bg-white border-2 p-5">
+          <div className="relative z-50 md:w-[600px] w-[370px] bg-white border-2 p-10 rounded-3xl">
             <Title addClass="text-[40px] text-center">search</Title>
             <input placeholder='search...' className='border w-full my-10'/>
             <ul>
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,21 +1,21 @@
-import {useState} from 'react'
+import {useState} from "react"
 
-import {FaUserAlt, FaShoppingCart, FaSearch} from 'react-icons/fa'
+import {FaUserAlt, FaShoppingCart, FaSearch} from "react-icons/fa"
+import {GiHamburgerMenu,GiCancel} from "react-icons/gi"
 
-import Logo from '../UI/Logo'
-import Search from '../UI/Search'
+import Logo from "../UI/Logo"
+import Search from "../UI/Search"
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
 
   return (
-    <div className='h-[5.5rem] bg-secondary'>
-      <div className='container mx-auto flex justify-between items-center h-full'>
-        <div>
-            <Logo/>
-        </div>
-        <div>
-          <ul className="flex gap-x-2 text-white">
+    <div className="h-[5.5rem] bg-secondary">
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        <Logo/>
+        <nav className={`${isMenuModal === true && "!grid place-content-center"} sm:flex hidden sm:bg-transparent bg-white sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black flex-col items-center`}>
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center ">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer transition-all ">
               <a href="">Home</a> 
             </li>
@@ -29,20 +29,33 @@ const Header = () => {
               <a href="">Book Table</a> 
             </li>
           </ul>
-        </div>
-        <div className='flex items-center gap-x-4 text-white'>
+          
+          {isMenuModal && (
+            <button 
+              className='absolute top-4 right-4 z-50' 
+              onClick={() => setIsMenuModal(false)}
+            >
+              <GiCancel size={25}/>
+            </button>
+          )}
+        </nav>
+        <div className="flex items-center gap-x-4 text-white">
           <a href="">
-            <FaUserAlt className='hover:text-primary transition-all'/>
+            <FaUserAlt className="hover:text-primary transition-all"/>
           </a> 
           <a href="">
-            <FaShoppingCart className='hover:text-primary transition-all'/>
+            <FaShoppingCart className="hover:text-primary transition-all"/>
           </a> 
           <button onClick={() => setIsSearchModal(true)}>
-            <FaSearch className='hover:text-primary transition-all'/>
+            <FaSearch className="hover:text-primary transition-all"/>
           </button> 
-          <a href="">
+          <a className="md:inline-block hidden" href="">
             <button className="btn-primary">Order Online</button>
           </a> 
+          <button className="md:hidden inline-block" onClick={() => setIsMenuModal(true)}>
+            <GiHamburgerMenu className="hover:text-primary transition-all"/>
+          </button>
+
         </div>
       </div>
       
