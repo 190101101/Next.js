abab3f6331f782c47fe2c08d7ebfff592e2c4cf2
190101101 <190101101@mail.ru>
2023-09-22 22:41:39 +0400

    21 Input Component

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -1,11 +1,15 @@
 import Campaigns from "@/components/Campaigns"
 import Carousel from "@/components/Carousel"
+import MenuWrapper from "../../components/product/MenuWrapper"
+import  About from '@/pages/About'
 
 const home = () => {
   return (
     <div className="">
       <Carousel/>
       <Campaigns/>
+      <MenuWrapper/>
+      <About/>
     </div>
   )
 }
