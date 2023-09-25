52a3f77bc6f18bee66c7a4082529e3709989c125
190101101 <190101101@mail.ru>
2023-09-25 23:05:02 +0400

    08 cart and order\35 Cart Page Table

--- pages/cart/cart.jsx
+++ pages/cart/cart.jsx
@@ -2,42 +2,68 @@ import Title from "@/components/ui/Title";
 import Image from "next/image";
 
 const Cart = () => {
+
   return (
-    <div className="min-h-[calc(100vh_-_300px)]">
-      <div className="flex justify-between">
-        <div className="min-h-[calc(100vh_-_300px)] items-center">
-          <table>
-            <thead>
-              <tr>
-                <th>product</th>
-                <th>extras</th>
-                <th>price</th>
-                <th>quantity</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <Image src="/images/f1.png" alt="" width={50} height={50} />
-                  <span>Good Pizza</span>
-                </td>
-                <td>
-                  <span>mayonez, aci, sos, ketcap</span>
-                </td>
-                <td>$20</td>
-                <td>1</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="bg-secondary min-h-[calc(100vh_-_300px)] flex-col justify-center p-8 text-white">
-          <Title addClass={"text-[40px]"}>Cart Total</Title>
-          <div className="flex flex-col ">
-            <span>Subtotal: $20</span>
-            <span>Discount: 0.00</span>
-            <span>Total: $20</span>
+    <div className="min-h-[calc(100vh_-_433px)]">
+      <div className="flex justify-between items-center md:flex-row flex-col">
+        <div className="md:min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full">
+          <div className="max-h-52 overflow-auto w-full">
+              <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
+                <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+                  <tr>
+                    <th scope="col" className="py-3 px-6">
+                      PRODUCT
+                    </th>
+                    <th scope="col" className="py-3 px-6">
+                      EXTRAS
+                    </th>
+                    <th scope="col" className="py-3 px-6">
+                      PRICE
+                    </th>
+                    <th scope="col" className="py-3 px-6">
+                      QUANTITY
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr className="transition-all bg-secondary border-gray-700 hover:bg-primary" >
+                      <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
+                        <Image
+                          src={'/images/f1.png'}
+                          alt=""
+                          width={50}
+                          height={50}
+                        />
+                        <span>Good Pizza</span>
+                      </td>
+                      <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">mayonez, aci, sos, ketcap</td>
+                      <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                        $20
+                      </td>
+                      <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                        1
+                      </td>
+                    </tr>
+                </tbody>
+              </table>
           </div>
-          <button className="btn-primary mt-4">Checkout now</button>
+        </div>
+        <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full   md:text-start !text-center">
+          <Title addClass="text-[40px]">CART TOTAL</Title>
+
+          <div className="mt-6">
+            <b>Subtotal: </b>10 <br />
+            <b className=" inline-block my-1">Discount: </b>$0.00 <br />
+            <b>Total: </b>10
+          </div>
+
+          <div>
+            <button
+              className="btn-primary mt-4 md:w-auto w-52"
+            >
+              CHECKOUT NOW!
+            </button>
+          </div>
         </div>
       </div>
     </div>
