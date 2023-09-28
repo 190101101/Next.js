036360a9caa1fe879c291dcc1a0e7b3969fa14bc
190101101 <190101101@mail.ru>
2023-09-29 02:02:02 +0400

    11 redux\48 Product Detail Price Calculation

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,29 +1,90 @@
 import Title from "@/components/ui/Title";
 import Image from "next/image";
+import { useState } from "react";
+
+const itemsExtra = [
+    {
+        id:1,
+        name:'extra 1',
+        price: 1,
+    },
+    {
+        id:2,
+        name:'extra 2',
+        price: 2,
+    },
+    {
+        id:3,
+        name:'extra 3',
+        price: 3,
+    },
+];
 
 const index = () => {
+    const [prices, setPrices] = useState([10,20,30]);
+    const [price, setPrice] = useState(prices[0]);
+    const [size, setSize] = useState(0);
+    const [extraItems, setExtraItems] = useState(itemsExtra);
+    const [extras, setExtras] = useState([]);
+
+    const handleSize = (sizeIndex) => {
+        const difference = prices[sizeIndex] - prices[size];
+        setSize(sizeIndex);
+        changePrice(difference);
+    }
+
+    const changePrice = (number) => {
+        setPrice(price + number);
+    }
+
+    const handleChange = (e, item) => {
+        const checked = e.target.checked;
+        
+        if(checked){
+            setExtras([...extras, item]);
+            changePrice(item.price);
+        }else{
+            setExtras(extras.filter((extra) => extra.id !== item.id));
+            changePrice(-item.price);
+        }
+    }
+
+    console.log(extras);
+
   return (
-    <div className="flex flex-wrap items-center h-screen gap-20">
-        <div className="relative md:flex-1 w-[80%] h-[80%] mx-20 py-20">
+    <div className="flex flex-wrap items-center md:h-[calc(100vh_-_80px)] gap-5">
+        <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto py-20">
             <Image src="/images/f1.png" alt="" layout="fill" objectFit="contain"/>
         </div>
         <div className="md:flex-1 md:text-start text-center">
             <Title addClass="text-6xl">Good Pizza</Title>
-            <span className="text-primary text-2xl font-bol underline underline-offset-2 my-4 inline-block">$10</span>
+            <span className="text-primary text-2xl font-bol underline underline-offset-2 my-4 inline-block">${price}</span>
             <p className="text-sm my-4 md:pr-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi repudiandae dolorem natus nesciunt placeat deleniti iusto, accusamus aut quaerat in ipsa aliquid, hic magni aliquam qui odio voluptate molestiae.</p>
             <div>
                 <h4 className="text-xl font-bold">Choose the size</h4>
-                <div className="flex items-center md:justify-start sm:justify-center gap-10">
-                    <div className="relative w-8 h-8">
-                        <Image src="/images/f1.png" alt="" layout="fill"/>
+                <div className="flex items-center justify-center md:justify-start gap-10">
+                    <div className="relative w-8 h-8 cursor-pointer">
+                        <Image src="/images/f1.png" alt="" layout="fill" 
+                            onClick={() => {
+                                handleSize(0);
+                            }}
+                        />
                         <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">Small</span>
                     </div>
-                    <div className="relative w-12 h-12">
-                        <Image src="/images/f1.png" alt="" layout="fill"/>
+                    <div className="relative w-12 h-12 cursor-pointer">
+                        <Image src="/images/f1.png" alt="" layout="fill" 
+                            onClick={() => {
+                                handleSize(1);
+                            }}
+                        />
                         <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">Middle</span>
                     </div>
-                    <div className="relative w-16 h-16">
-                        <Image src="/images/f1.png" alt="" layout="fill"/>
+                    <div className="relative w-16 h-16 cursor-pointer">
+                        <Image src="/images/f1.png" alt="" layout="fill" 
+                            onClick={() => {
+                                handleSize(2);
+                            }}
+                        />
                         <span className="absolute top-0 -right-4 text-xs bg-primary rounded-full px-[5px] font-medium">Large</span>
                     </div>
                 </div>
@@ -33,19 +94,16 @@ const index = () => {
                 <div className="">
                     <p className="text-xl-2 font-bold">Choose addtional ingredients</p>
                 </div>
-                <div className="flex items-center md:justify-start sm:justify-center gap-x-4 my-6">
-                    <label className="flex items-center gap-x-1">
-                        <input type="checkbox" className="w-5 h-5 accent-primary" />
-                        <span className="text-sm font-semibold">ketcup</span>
+                <div className="flex items-center justify-center md:justify-start gap-x-4 my-6">
+                    {extraItems.map((item) => 
+                    <label key={item.id} className="flex items-center gap-x-1">
+                        <input type="checkbox" className="w-5 h-5 accent-primary" 
+                            value={item.name}
+                            onChange={(e) => handleChange(e, item)}
+                        />
+                        <span className="text-sm font-semibold">{item.name}</span>
                     </label>
-                    <label className="flex items-center gap-x-1">
-                        <input type="checkbox" className="w-5 h-5 accent-primary" />
-                        <span className="text-sm font-semibold">ketcup</span>
-                    </label>
-                    <label className="flex items-center gap-x-1">
-                        <input type="checkbox" className="w-5 h-5 accent-primary" />
-                        <span className="text-sm font-semibold">ketcup</span>
-                    </label>
+                    )}
                 </div>
                 <button className="btn-primary">add to cart</button>
             </div>
